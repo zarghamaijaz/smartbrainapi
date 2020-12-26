99a79991d7346c9dfe2be2e6b203224ab06e6194
Zargham Aijaz <67057109+zarghamaijaz@users.noreply.github.com>
2020-12-26 22:02:09 +0500

    changing database port

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image= require('./controllers/image')
 const db=knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-sinuous-23940',
-    user : 'postgres',
-    password : 'zargham',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl:true
   }
 });
 

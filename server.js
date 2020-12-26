a715d60dd2d42d88d008bce633cb757ba1a5b95b
Zargham Aijaz <67057109+zarghamaijaz@users.noreply.github.com>
2020-12-26 21:57:36 +0500

    added database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image= require('./controllers/image')
 const db=knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-sinuous-23940',
     user : 'postgres',
     password : 'zargham',
     database : 'smart-brain'

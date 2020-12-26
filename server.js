93e33ec06e50802d60925f76a44b0afb8fa9148f
Zargham Aijaz <67057109+zarghamaijaz@users.noreply.github.com>
2020-12-26 11:38:53 +0000

    starting files

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile= require('./controllers/profile');
+const image= require('./controllers/image')
+const db=knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'zargham',
+    database : 'smart-brain'
+  }
+});
+
+// db.select('*').from('users').then(data=>{
+// 	console.log(data);
+// });
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port=3000;
+
+app.get('/',(req,res)=>{
+	res.json('Hi there!');
+	});
+
+app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)});
+
+app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)});
+
+app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)});
+
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)});
+
+
+
+app.listen(port,()=>{
+	console.log(`app in running on port ${port}`);
+});
